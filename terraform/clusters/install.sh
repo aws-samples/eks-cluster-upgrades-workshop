71d8c6cf8d17f3877cb84701d7e91681887386c1
lusoal <lucasdu@amazon.com>
2023-06-01 21:47:09 -0500

    adjusted install script

--- terraform/clusters/install.sh
+++ terraform/clusters/install.sh
@@ -3,7 +3,7 @@ echo "Executing Terraform"
 
 terraform init
 
-terraform plan
+terraform plan -var="git_password=$1" -var="git_username=$2" -var="git_url=$3" -var="git_branch=$4" -var="aws_region=$5"
 
 # TODO: add aws_region on terraform apply command
 terraform apply --auto-approve -var="git_password=$1" -var="git_username=$2" -var="git_url=$3" -var="git_branch=$4" -var="aws_region=$5" --auto-approve
